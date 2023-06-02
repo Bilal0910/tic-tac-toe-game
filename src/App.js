6134ed3b017d0e41d0a7eb3d72ebe403ab2dd606
bilal-nadeem <bilalnadeem4u@gmail.com>
2023-06-02 23:12:14 +0500

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import './App.css';
 function App() {
   return (
-    <div>
-      <h2>WELCOME TO THE TIC TAC TOE</h2>
-      <button className="square">X</button>
+    <div className='back'>
+      <button className="square">1</button>
+      <button className="square">2</button>
+      <button className="square">3</button>
+      <button className="square">4</button>
+      <button className="square">5</button>
+      <button className="square">6</button>
+      <button className="square">7</button>
+      <button className="square">8</button>
+      <button className="square">9</button>
     </div>
 
 
